ae57c16265e364da86560b7b55025ea0a4f92409
vagrant <vagrant@homestead>
2019-10-11 01:46:11 +0000

    change welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -36,7 +36,7 @@
 <body>
 <div class="container">
     <div class="content">
-        <div class="title">Hello Laravel! - by Summer</div>
+        <div class="title">Hello Laravel! - by FiveEggs</div>
     </div>
 </div>
 </body>
